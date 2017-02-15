3f6ea8d2683383c5a2a25e17de8caf478f87d911
iTimeTraveler <xuewenlong.2008@gmail.com>
2017-02-15 19:18:34 +0800

    igraph度分布

--- cytoscape_neo4j-master/findcommunities.py
+++ cytoscape_neo4j-master/findcommunities.py
@@ -2,7 +2,7 @@
 #coding:utf8
 
 from models import graph
-from igraph import Graph as IGraph
+from igraph import plot, Graph as IGraph
 
 
 # def findCommunities():
@@ -15,30 +15,41 @@ RETURN c1.name, c2.name, r.weight AS weight
 
 ig = IGraph.TupleList(graph.run(query), weights=True)
 
-pageRank = ig.pagerank()
-pgvs = []
-for p in zip(ig.vs, pageRank):
-    print(p)
-    pgvs.append({"name": p[0]["name"], "pg": p[1]})
-pgvs
-write_clusters_query = '''
-UNWIND {nodes} AS n
-MATCH (c:Character) WHERE c.name = n.name
-SET c.pagerank = n.pg
-'''
-graph.run(write_clusters_query, nodes=pgvs)
+# pageRank = ig.pagerank()
+# pgvs = []
+# for p in zip(ig.vs, pageRank):
+#     print(p)
+#     pgvs.append({"name": p[0]["name"], "pg": p[1]})
+# pgvs
+# write_clusters_query = '''
+# UNWIND {nodes} AS n
+# MATCH (c:Character) WHERE c.name = n.name
+# SET c.pagerank = n.pg
+# '''
+# graph.run(write_clusters_query, nodes=pgvs)
+#
+#
+#
+# # 随机游走的社区发现算法
+# clusters = IGraph.community_walktrap(ig, weights="weight").as_clustering()
+# nodes = [{"name": node["name"]} for node in ig.vs]
+# for node in nodes:
+#     idx = ig.vs.find(name=node["name"]).index
+#     node["community"] = clusters.membership[idx]
+# write_clusters_query = '''
+# UNWIND {nodes} AS n
+# MATCH (c:Character) WHERE c.name = n.name
+# SET c.community = toInt(n.community)
+# '''
+# graph.run(write_clusters_query, nodes=nodes)
 
 
 
-# 随机游走的社区发现算法
-clusters = IGraph.community_walktrap(ig, weights="weight").as_clustering()
-nodes = [{"name": node["name"]} for node in ig.vs]
-for node in nodes:
-    idx = ig.vs.find(name=node["name"]).index
-    node["community"] = clusters.membership[idx]
-write_clusters_query = '''
-UNWIND {nodes} AS n
-MATCH (c:Character) WHERE c.name = n.name
-SET c.community = toInt(n.community)
-'''
-graph.run(write_clusters_query, nodes=nodes)
+
+# 度分布
+dd = ig.degree_distribution()
+ig.vs.degree()
+ig.vs['name']
+ig.degree(type="in")
+plot(dd)
+print (dd)
